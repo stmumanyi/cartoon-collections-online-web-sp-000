3880e7411f6c7ec86e1df090a95b534f343b9403
Simba Mumanyi <simbamumanyi@gmail.com>
2020-09-16 14:07:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,24 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  i = 0 
+  while i < array.length 
+  puts "#{i + 1}. #{array[i]}"
+  i += 1 
+end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  captain = []
+  i = 0 
+  while i < array.length 
+  captain << array[i].capitalize + "!"
+  i += 1 
+end
+captain
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  i = 0 
+  if array.any? {|i|
 end
 
 def find_the_cheese# code an argument here
